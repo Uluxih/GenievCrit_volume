aea6ab1c7039e80256bcba4465374e57472a6ecc
Uluxih <ivsmag@vk.com>
2024-10-13 15:55:03 +0300

    скачок к ООП

--- main.py
+++ main.py
@@ -1,77 +1,82 @@
 import math
 import numpy as np
-from manim import *
-import pyautocad as pyA
-from pyautocad import Autocad, APoint, ACAD
 import matplotlib.pyplot as plt
 
 def lm_xyz(l, m, n, l1, l2, l3):
     return (l*l1+m*l2+n*l3)
 
 def get_crit_result(tau, Cm):
-    return tau<Cm
+    return tau<Ccm
 
-C_x=1/5
-C_y=1/5
-C_z=1/5
+class Stress_point:
+    def __init__(self, tensor, plates):
+        self.tensor=tensor
+        self.plates=plates
+class Tensor:
+    def __init__(self, sigma1, sigma2, sigma3):
+        self.sigma1=sigma1
+        self.sigma2=sigma2
+        self.sigma3=sigma3
+class Plate:
+    def __init__(self, strength_plate, stress_state_plate, lmn):
+        self.strength_plate=strength_plate
+        self.stress_state_plate=stress_state_plate
+        self.lmn=lmn
+class Strength_plate:
+    def __init__(self, cohesion, k, normal_resist):
+        self.cohesion = cohesion
+        self.k=k
+        self.normal_resist=normal_resist
+class Stress_state_plate:
+    def __init__(self, normal, shear):
+        self.normal=normal
+        self.shear=shear
+C_x=4
+C_y=0.5
+C_z=0.2
+q=0.2
 
-sigma1=2
-sigma2=2
-sigma3=2
+R_x=4.5
+R_y=3.5
+R_z=5
 
 l=0
 m=0
 n=math.sqrt(1-l**2-m**2)
 
-# l1x=0.707106781
-# l2x=-0.707106781
-# l3x=math.sqrt(1-l1x**2-l2x**2)
-#
-# l1y=0.707106781
-# l2y=0.707106781
-# l3y=math.sqrt(1-l1y**2-l2y**2)
-#
-# l1z=math.sqrt(1-l1x**2-l1y**2)
-# l2z=math.sqrt(1-l2x**2-l2y**2)
-# l3z=math.sqrt(1-l3x**2-l3y**2)
+# l1x, l2x, l3x = 1, 0, 0
+# l1y, l2y, l3y = 0, 1, 0
+# l1z, l2z, l3z = 0, 0, 1
 
-l1x=1
-l2x=0
-l3x=0
+# l1x, l2x, l3x = 0.71, 0.71, 0
+# l1y, l2y, l3y = -0.71, 0.71, 0
+# l1z, l2z, l3z = 0, 0, 1
 
-l1y=0
-l2y=1
-l3y=0
-
-l1z=0
-l2z=0
-l3z=1
-
-#print(l1x,"\t", l2x,"\t",l3x)
-#print(l1y,"\t",l2y,"\t",l3y)
-#print(l1z,"\t",l2z,"\t",l3z)
-
-l_mx=lm_xyz(l, m, n, l1x, l2x, l3x)
-l_my=lm_xyz(l, m, n, l1y, l2y, l3y)
-l_mz=lm_xyz(l, m, n, l1z, l2z, l3z)
-#print(l_mx, l_my, l_mz, math.sqrt(l_mx**2+l_my**2+l_mz**2) )
-
-C_m=C_x*l_mx**2+C_y*l_my**2+C_z*l_mz**2
+l1x, l2x, l3x = 0.92, 0.3, -0.24
+l1y, l2y, l3y = -0.11, 0.81, 0.57
+l1z, l2z, l3z = 0.37, -0.5, 0.78
 
 def get_lmn():
     lmn = []
     l=0
     m=0
     n=0
+    d=0.1
     while(l<1):
         m=0
-        while(m<1 and l**2+m**2+n**2<=1):
-            n=math.sqrt(1-l**2-m**2)
-            lmn.append((l,m,n))
-            m=m+0.05
+        while(m<1):
             n=0
-        l=l+0.05
-    return lmn
+            while(n<1):
+                lmn.append((l,m,n))
+                n+=d
+            m+=d
+        l+=d
+    true_lmn=[]
+    for i in lmn:
+        if (i[0] ** 2 + i[1] ** 2 + i[2] ** 2 <= 1):
+            true_lmn.append(i)
+    return true_lmn
+
 def get_Cm(lmn):
     lmn_Cm={}
     lmn_taum={}
@@ -96,6 +101,7 @@ def get_taum(lmn, sigs):
         tau=math.sqrt(((sigma1-sigma2)*l*m)**2+((sigma3-sigma2)*m*n)**2+((sigma1-sigma3)*l*n)**2)
         lmn_taum[lmn_comb]=tau
     return lmn_taum
+
 def get_sigm(lmn, sigs):
     sigma1=sigs[0]
     sigma2 = sigs[1]
@@ -109,28 +115,53 @@ def get_sigm(lmn, sigs):
         #print(lmn_comb, sigma)
         lmn_sigm[lmn_comb]=sigma
     return lmn_sigm
+
 def get_res(lmn, lmn_taum, lmn_sigm, lmn_Cm):
     danger_plate = []
     for lmn_comb in lmn:
         #print(lmn_comb, lmn_taum[lmn_comb]/lmn_sigm[lmn_comb], lmn_Cm[lmn_comb])
         if (lmn_sigm[lmn_comb]!=0):
-            if lmn_taum[lmn_comb]/lmn_sigm[lmn_comb]>=lmn_Cm[lmn_comb]:
-                #print("Сомнительно, но окэй", lmn_taum[lmn_comb]/lmn_sigm[lmn_comb]-lmn_Cm[lmn_comb])
+            if lmn_taum[lmn_comb]>=lmn_Cm[lmn_comb]+q*lmn_sigm[lmn_comb]:
                 danger_plate.append(lmn_comb)
+                continue
                 #print(i)
+            # l_mx = lm_xyz(lmn_comb[0], lmn_comb[1], lmn_comb[2], l1x, l2x, l3x)
+            # l_my = lm_xyz(lmn_comb[0], lmn_comb[1], lmn_comb[2], l1y, l2y, l3y)
+            # l_mz = lm_xyz(lmn_comb[0], lmn_comb[1], lmn_comb[2], l1z, l2z, l3z)
+            # R_m = R_x * l_mx ** 2 + R_y * l_my ** 2 + R_z * l_mz ** 2
+            # if lmn_sigm[lmn_comb]>=R_m:
+            #     danger_plate.append(lmn_comb)
     return danger_plate
+
+def get_res_dict(lmn, lmn_taum, lmn_sigm, lmn_Cm):
+    dangPlate_lmn = {}
+    for lmn_comb in lmn:
+        #print(lmn_comb, lmn_taum[lmn_comb]/lmn_sigm[lmn_comb], lmn_Cm[lmn_comb])
+        if (lmn_sigm[lmn_comb]!=0):
+            if lmn_taum[lmn_comb]>=lmn_Cm[lmn_comb]+q*lmn_sigm[lmn_comb]:
+                danger_plate.append(lmn_comb)
+                continue
+                #print(i)
+            # l_mx = lm_xyz(lmn_comb[0], lmn_comb[1], lmn_comb[2], l1x, l2x, l3x)
+            # l_my = lm_xyz(lmn_comb[0], lmn_comb[1], lmn_comb[2], l1y, l2y, l3y)
+            # l_mz = lm_xyz(lmn_comb[0], lmn_comb[1], lmn_comb[2], l1z, l2z, l3z)
+            # R_m = R_x * l_mx ** 2 + R_y * l_my ** 2 + R_z * l_mz ** 2
+            # if lmn_sigm[lmn_comb]>=R_m:
+            #     danger_plate.append(lmn_comb)
+    return danger_plate
+
 sigs=[]
-sig1=0
+sig1=-2
 lmn=get_lmn()
 while (sig1<5):
-    sig2 = 0
+    sig2 = -2
     while (sig2<5):
-        sig3 = 0
+        sig3 = -2
         while (sig3<5):
             sigs.append((sig1, sig2, sig3))
-            sig3+=0.2
-        sig2+=0.2
-    sig1+=0.2
+            sig3+=0.33
+        sig2+=0.33
+    sig1+=0.33
 print (len(sigs))
 danger_point=[]
 sigs1=[]
@@ -146,7 +177,10 @@ for sig in sigs:
         sigs1.append(sig[0])
         sigs2.append(sig[1])
         sigs3.append(sig[2])
-print(sigs)
+
+
+#Быстрый, но непонятный алгоритм
+
 
 fig = plt.figure()
 
